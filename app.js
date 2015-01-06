d350bccd4b127f7044afecc1bcccaf0b861ea794
LocNT <nguyentienloc.it@gmail.com>
2015-01-06 13:25:33 +0700

    20150106

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var path = require('path');
 var persons = require('./routers/personRouter');
 
 var app = express();
-var http = require('http').Server(app);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //mapping url
@@ -19,6 +18,6 @@ app.use('/person', persons);
 
 // Start server
 app.set('port', (process.env.PORT || 5000));
-http.listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
 	console.log("Node app is running at localhost:" + app.get('port'));
 });
